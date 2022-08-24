00191123b1b9467b7e42c1a11d764d339fcd3a39
PascoPrzyborowski <unity01@gmx.net>
2022-08-24 14:06:55 +0200

    ex11

---
+++ exercise11.py
@@ -0,0 +1,49 @@
+
+
+
+#Task1
+
+# text = """Berlin is a world city of culture, politics, media and science."""
+# print(len(text))
+
+#Task2
+
+# text = """Berlin is a world city of culture, politics, media and science."""
+# print(text[0:1]+" "+text[-1:])
+
+
+#Task3
+
+
+# text = """Berlin is a world city of culture, politics, media and science."""
+# print("First three charakters: " + text.upper()[0:3])
+
+
+
+#Task4
+
+# text = """Berlin is surrounded by the State of Brandenburg 
+# and contiguous with Potsdam, Brandenburg's capital """
+# print("B appears: " + str(text.count("B")) + " "+"times in the text")
+
+#Task5
+
+# text = """Berlin straddles the banks of the Spree, which flows into the Havel
+#  (a tributary of the Elbe) in the western borough of Spandau."""
+# print(len(text))
+# print("Last ten Characters are: "+ text[-10:])
+
+#Task6
+
+# text = "---Python programming---"
+# print()
+# print(text)
+# print()
+# print(text.strip("-"))
+
+#Task7
+
+first_name = "Pasco"
+last_name = "Przyborowski"
+print("Firstname: " +first_name +"\n" + "Lastname: " + last_name)
+
